3c8ee08418dc0714ede0b2a303eeba498237409b
MetroCheatPBO <64860864+MetroCheatPBO@users.noreply.github.com>
2020-05-21 13:42:11 +0700

    Create MenuV1.lua

---
+++ MenuV1.lua
@@ -0,0 +1,292 @@
+------------------------------------------------------
+--SUBSTRINGS
+------------------------------------------------------
+on = "  ◤𝙾𝙽◢"
+off = "◤𝙾𝙵𝙵◢"
+
+------------------------------------------------------
+--STRINGS
+------------------------------------------------------
+string1 = off
+string2 = off
+string3 = off
+string4 = off
+string5 = off
+string6 = off
+string7 = off
+string8 = off
+------------------------------------------------------
+--MENU FOLDER
+------------------------------------------------------
+gg.alert("𝚂𝚌𝚛𝚒𝚙𝚝 𝙼𝚎𝚝𝚛𝚘𝙲𝚑𝚎𝚊𝚝_𝙿𝙱𝙾™ 𝙼𝚎𝚗𝚞 𝙾𝚗-𝙾𝚏𝚏 ")
+gg.alert("𝙿𝚄𝙱𝙶 𝙶𝙻𝙾𝙱𝙰𝙻 - 𝚂𝙴𝙰𝚂𝙾𝙽 𝟷𝟹\n\n𝙶𝚄𝙽𝙰𝙺𝙰𝙽 𝙰𝙺𝚄𝙽 𝚃𝚄𝙼𝙱𝙰𝙻 𝚃𝙴𝚁𝙻𝙴𝙱𝙸𝙷 𝙳𝙰𝙷𝚄𝙻𝚄\n𝙿𝙴𝚁𝚃𝙰𝙼𝙰 𝙻𝙾𝙶𝙸𝙽 - 𝙰𝙺𝚃𝙸𝙵𝙺𝙰𝙽 𝙵𝙸𝚃𝚄𝚁' 𝙻𝙴𝙱𝙸𝙷 𝙱𝙰𝙸𝙺 𝙳𝙸 𝚃𝚁𝙰𝙸𝙽𝙴𝚁 𝙼𝙾𝙳𝙴\n𝙿𝙻𝙰𝚈𝙸𝙽𝙶 𝙱𝚁𝚄𝚃𝙰𝙻 𝚃𝙸𝙳𝙰𝙺 𝙳𝙸𝚂𝙰𝚁𝙰𝙽𝙺𝙰𝙽\n\n𝚁𝚎𝚐𝚊𝚛𝚍𝚜 𝙲𝚛𝚎𝚊𝚝𝚎𝚍  𝙱𝚢 𝙼𝙲𝙿𝙱𝙾™ - @𝚐𝙷𝚎𝚜𝚜𝚜_𝙱𝚘𝚢𝚜 ")
+
+function main()
+menu = gg.choice({
+	string1.."             ✎﹏ 𝙻𝚎𝚜𝚜 𝚁𝚎𝚌𝚘𝚒𝚕",
+	string2.."             ✎﹏ 𝙰𝚒𝚖𝚕𝚘𝚌𝚔 𝙴𝚗𝚎𝚖𝚢",
+	string3.."             ✎﹏ 𝙰𝚞𝚝𝚘 𝙷𝚂 - 𝙼𝙱 𝟻𝟶%",
+	string4.."             ✎﹏ 𝙰𝚞𝚝𝚘 𝙷𝚂 - 𝙼𝙱 𝟿𝟶%",
+	string5.."             ✎﹏ 𝙰𝚗𝚝𝚎𝚗𝚊 𝙿𝚕𝚊𝚢𝚎𝚛𝚜",
+	string6.."             ✎﹏ 𝙰𝚗𝚝𝚑𝚎𝚗𝚊 𝙶𝚑𝚒𝚕𝚕𝚒𝚎",
+	string7.."             ✎﹏ 𝚆𝚒𝚍𝚎 𝚅𝚒𝚎𝚠𝚜 𝟼𝟶%",
+	string8.."             ✎﹏ 𝙱𝚕𝚊𝚌𝚔 𝚂𝚔𝚢 𝙴𝚡𝚢𝚗𝚘𝚜",
+	"🛡️ Bypass Reconnect Data 🛡️ ",
+	"»ᴇxɪᴛ«"}, nil, "𝙼𝚎𝚝𝚛𝚘𝙲𝚑𝚎𝚊𝚝_𝙿𝙱𝙾™ - 𝙼𝚎𝚗𝚞 𝙾𝚗-𝙾𝚏𝚏")
+
+------------------------------------------------------
+--CALL STRINGS & MENU
+------------------------------------------------------
+	if menu == 1 then
+	if string1 == off then
+	string1 = on
+	else
+	string1 = off
+	end
+	LSR()
+--2nd-- ᴄᴏᴘʏʀɪɢʜᴛ ® | 𝙱𝚢 𝙼𝙲𝙿𝙱𝙾™ - @𝚐𝙷𝚎𝚜𝚜𝚜_𝙱𝚘𝚢𝚜
+	elseif menu == 2 then
+	if string2 == off then
+	string2 = on
+	else
+	string2 = off
+	end
+	aimlock()
+--3rd-- ᴄᴏᴘʏʀɪɢʜᴛ ® | 𝙱𝚢 𝙼𝙲𝙿𝙱𝙾™ - @𝚐𝙷𝚎𝚜𝚜𝚜_𝙱𝚘𝚢𝚜
+	elseif menu == 3 then
+	if string3 == off then
+	string3 = on
+	else
+	string3 = off
+	end
+	HSMB50()
+--4th-- ᴄᴏᴘʏʀɪɢʜᴛ ® | 𝙱𝚢 𝙼𝙲𝙿𝙱𝙾™ - @𝚐𝙷𝚎𝚜𝚜𝚜_𝙱𝚘𝚢𝚜
+	elseif menu == 4 then
+	if string4 == off then
+	string4 = on
+	else
+	string4 = off
+	end
+	HSMB99()
+--5th	
+	elseif menu == 5 then
+	if string5 == off then
+	string5 = on
+	else
+	string5 = off
+	end
+	AntenaPlyers()
+--6th	
+	elseif menu == 6 then
+	if string6 == off then
+	string6 = on
+	else
+	string6 = off
+	end
+	AntenaGilie()
+--7th	
+	elseif menu == 7 then
+	if string7 == off then
+	string7 = on
+	else
+	string7 = off
+	end
+	view60()
+--8th	
+	elseif menu == 8 then
+	if string8 == off then
+	string8 = on
+	else
+	string8 = off
+	end
+	BsKy()
+--9th
+	elseif menu == 9 then
+	rest()	
+--Last Exit-- ᴄᴏᴘʏʀɪɢʜᴛ ® | 𝙱𝚢 𝙼𝙲𝙿𝙱𝙾™ - @𝚐𝙷𝚎𝚜𝚜𝚜_𝙱𝚘𝚢𝚜
+	elseif menu == 10 then
+	print("ᴄᴏᴘʏʀɪɢʜᴛ ® | 𝙱𝚢 𝙼𝚎𝚝𝚛𝚘𝙲𝚑𝚎𝚊𝚝_𝙿𝙱𝙾™ - @𝚐𝙷𝚎𝚜𝚜𝚜_𝙱𝚘𝚢𝚜 ")
+	os.exit()
+	end
+	end
+
+
+------------------------------------------------------
+--CALL FUNGSIONAL HACK
+------------------------------------------------------
+function LSR()
+if string1 == on then
+gg.clearResults()
+gg.setRanges(gg.REGION_C_DATA)
+gg.searchNumber("-2.2673448e24;-1.36203639e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-1.36203639e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+LSRcloff = gg.getResults(99)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.toast("Less Recoil Active ")
+else
+gg.setValues(LSRcloff)
+gg.toast("OFF")
+end
+end
+--2nd
+function aimlock()
+if string2 == on then
+gg.clearResults()
+gg.setRanges(gg.REGION_C_DATA)
+gg.searchNumber("1,324,366,404", gg.TYPE_DWORD,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("1,324,366,404", gg.TYPE_DWORD,false,gg.SIGN_EQUAL,0, -1)
+aLockoff = gg.getResults(80)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("AimLock Active ")
+else
+gg.setValues(aLockoff)
+gg.toast("OFF")
+end
+end
+--3rd
+function HSMB50()
+if string3 == on then
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+hs5off = gg.getResults(10)
+gg.editAll("150", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("HEADSHOT - MB 50% Active")
+else
+gg.setValues(hs5off)
+gg.toast("OFF")
+end
+end	
+--4rd
+function HSMB99()
+if string4 == on then
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+hsoff = gg.getResults(10)
+gg.editAll("250", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("HEADSHOT - MB 99% Active")
+else
+gg.setValues(hsoff)
+gg.toast("OFF")
+end
+end	
+--5th
+function AntenaPlyers()
+if string5 == on then
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("88.50576019287F;87.27782440186F;-100.91194152832F;1F::13", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("88.50576019287F;87.27782440186F;1F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+Antoff = gg.getResults(6)
+gg.editAll("1.96875;1.96875;999;1.96875;1.96875;999", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Antena Active")
+else
+gg.setValues(Antoff)
+gg.toast("OFF")
+end
+end
+--6th
+function AntenaGilie()
+if string6 == on then
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("0.98900693655~0.98900723457;0.14786802232~0.14786840975;1.1920926e-7::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResultCount()
+gg.searchNumber("0.98900693655~0.98900723457", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+AntGoff = gg.getResults(100)
+gg.editAll("16000", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Antena Ghillie Active")
+else
+gg.setValues(AntGoff)
+gg.toast("OFF")
+end
+end
+--7th
+function view60()
+if string7 == on then
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("220;178;15 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("220", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+v60off = gg.getResults(300)
+gg.editAll("438", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Views Wide 60 Active")
+else
+gg.setValues(v60off)
+gg.toast("OFF")
+end
+end
+--8th
+function BsKy()
+if string8 == on then
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("100;403200::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("100", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+bskoff = gg.getResults(50)
+gg.editAll("-99", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Black Sky Exynos Active")
+else
+gg.setValues(bskoff)
+gg.toast("OFF")
+end
+end	
+--9th
+function rest()
+  gg.clearResults()
+  gg.setRanges(8)
+  gg.searchNumber("1.3132476e-38;1.956023e-35;1.1754945e-37::9", 16, false, 536870912, 0, -1)
+  if gg.getResultCount() == 0 then
+  else
+    gg.searchNumber("1.1754945e-37", 16, false, 536870912, 0, -1)
+    table.insert(ODC, gg.getResults(21))
+    gg.editAll("0", 16)
+    table.insert(EDC, gg.getResults(21))
+    gg.toast("Disconect Enabled")
+    gg.clearResults()
+    gg.sleep(500)
+    for i = #ODC, 1, -1 do
+      gg.setValues(ODC[i])
+    end
+    gg.toast("Disconect Disabled")
+  end
+end
+function ONDC()
+  for i = #EDC, 1, -1 do
+    gg.setValues(EDC[i])
+  end
+  gg.sleep(1000)
+  for i = #ODC, 1, -1 do
+    gg.setValues(ODC[i])
+  end
+  gg.toast("Disconect Enabled")
+end
+function OFFDC()
+  for i = #ODC, 1, -1 do
+    gg.setValues(ODC[i])
+  end
+  gg.toast("Disconect Disabled")
+end
+
+ODC = {}
+EDC = {}
+------------------------------------------------------
+--Loop
+------------------------------------------------------
+	while true do
+	if gg.isVisible() then
+	gg.setVisible(false)
+	main()
+	end
+	end
+------------------------------------------------------	
+--End
+------------------------------------------------------
