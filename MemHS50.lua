bea68adc7cf0e9e23689371fde9eeee6b53c22ac
MetroCheatPBO <64860864+MetroCheatPBO@users.noreply.github.com>
2020-05-22 15:25:15 +0700

    Create MemHS50.lua

---
+++ MemHS50.lua
@@ -0,0 +1,81 @@
+function split(szFullString, szSeparator)
+local nFindStartIndex = 1 
+local nSplitIndex = 1 
+local nSplitArray = {} while true do 
+local 
+nFindLastIndex = string.find(szFullString, szSeparator, nFindStartIndex) 
+if not nFindLastIndex then 
+nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, string.len(szFullString)) 
+break end 
+nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, nFindLastIndex - 1) 
+nFindStartIndex = nFindLastIndex + string.len(szSeparator) 
+nSplitIndex = nSplitIndex + 1 end return 
+nSplitArray end function 
+xgxc(szpy, qmxg) for x = 1, #(qmxg) do 
+xgpy = szpy + qmxg[x]["offset"] xglx = qmxg[x]["type"] 
+xgsz = qmxg[x]["value"] 
+gg.setValues({[1] = {address = xgpy, flags = xglx, value = xgsz}}) 
+xgsl = xgsl + 1 end end function 
+xqmnb(qmnb) 
+gg.clearResults() 
+gg.setRanges(qmnb[1]["memory"]) 
+gg.searchNumber(qmnb[3]["value"], qmnb[3]["type"]) 
+if gg.getResultCount() == 0 then 
+gg.toast(qmnb[2]["name"] .. "开启失败")
+else 
+gg.refineNumber(qmnb[3]["value"], qmnb[3]["type"]) 
+gg.refineNumber(qmnb[3]["value"], qmnb[3]["type"]) 
+gg.refineNumber(qmnb[3]["value"], qmnb[3]["type"]) 
+if gg.getResultCount() == 0 then 
+gg.toast(qmnb[2]["name"] .. "开启失败") 
+else 
+sl = gg.getResults(999999) 
+sz = gg.getResultCount() 
+xgsl = 0 if sz > 999999 then 
+sz = 999999 end for i = 1, sz do 
+pdsz = true for v = 4, #(qmnb) do if 
+pdsz == true then 
+pysz = {} pysz[1] = {} pysz[1].address = sl[i].address + qmnb[v]["offset"] 
+pysz[1].flags = qmnb[v]["type"] 
+szpy = gg.getValues(pysz) 
+pdpd = qmnb[v]["lv"] .. ";" .. szpy[1].value szpd = split(pdpd, ";") 
+tzszpd = szpd[1] 
+pyszpd = szpd[2] 
+if tzszpd == pyszpd then 
+pdjg = true pdsz = true else 
+pdjg = false pdsz = false end end end 
+if pdjg == true then 
+szpy = sl[i].address xgxc(szpy, qmxg) 
+xgjg = true end end 
+if xgjg == true then 
+gg.toast(qmnb[2]["name"] .. "开启成功,共修改" .. xgsl .. "条数据") 
+else 
+gg.toast(qmnb[2]["name"] .. "开启失败") 
+end 
+end 
+end 
+end
+
+
+qmnb = {
+{["memory"] = 32},
+{["name"] = "AUTO HEADSHOT  50%"},
+{["value"] = 30.5, ["type"] = 16},
+{["lv"] = 25.0, ["offset"] = -4, ["type"] = 16},
+}
+qmxg = {
+{["value"] = 349, ["offset"] = 0, ["type"] = 16},
+
+}
+xqmnb(qmnb)
+qmnb = {
+{["memory"] = 39},
+{["name"] = "AUTO HEADSHOT  50%"},
+{["value"] = 23.0, ["type"] = 16},
+{["lv"] = 25.0, ["offset"] = 4, ["type"] = 16},
+}
+qmxg = {
+{["value"] = 100, ["offset"] = 4, ["type"] = 16},
+
+}
+xqmnb(qmnb)
