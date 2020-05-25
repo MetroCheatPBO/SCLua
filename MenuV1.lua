0a70f85794eaa907f0d613114e0567e8ac55cfa5
MetroCheatPBO <64860864+MetroCheatPBO@users.noreply.github.com>
2020-05-25 22:20:47 +0700

    Update MenuV1.lua

--- MenuV1.lua
+++ MenuV1.lua
@@ -110,8 +110,94 @@ menu = gg.choice({
 	end
 	end
 
-
-------------------------------------------------------
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+gg.alert("Bypass 1 Done")
+if 100 < sl then
+  sl = 100
+end--Ꮲsᴀʏᴄʜᴏ⸙
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end--Ꮲsᴀʏᴄʜᴏ⸙
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+gg.alert("Bypass 2 Done")
+if 100 < sl then
+  sl = 100
+end--Ꮲsᴀʏᴄʜᴏ⸙
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end--Ꮲsᴀʏᴄʜᴏ⸙
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+gg.alert("Bypass 3 Done")
+if 100 < sl then
+  sl = 100
+end--Ꮲsᴀʏᴄʜᴏ⸙
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end--Ꮲsᴀʏᴄʜᴏ⸙
+gg.clearResults()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+gg.alert("Bypass 4 Done")
+if 100 < sl then
+  sl = 100
+end--Ꮲsᴀʏᴄʜᴏ⸙
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end--Ꮲsᴀʏᴄʜᴏ⸙
+gg.toast("Bypass Active")
+gg.clearResults()
+-----------------------------------------------------
 --CALL FUNGSIONAL HACK
 ------------------------------------------------------
 function LSR()
